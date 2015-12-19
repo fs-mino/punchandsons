b1586a43e2b18ee45fbcd789428e918d70157c48
西村奈央子のMacAir <naoko.nishimura1018@gmail.com>
2015-12-19 17:02:20 +0900

    all,/mock/をrootに移動

---
+++ config.rb
@@ -0,0 +1,20 @@
+# Require any additional compass plugins here.
+ 
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+ 
+output_style = :expanded
+# output_style = :nested
+# output_style = :compact
+# output_style = :compressed
+ 
+relative_assets = true
+line_comments = false
+ 
+preferred_syntax = :sass
+ 
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
